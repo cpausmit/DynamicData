5d681bdeeade6cc064e32f11677d5f72952bcb2b
Christoph Paus <paus@mit.edu>
2014-02-20 00:41:21 -0500

    Update setup.sh

--- Cinderella/setup.sh
+++ Cinderella/setup.sh
@@ -1,6 +1,5 @@
 export CINDERELLA_DEBUG=""
 
 export CINDERELLA_DATA="/mnt/hadoop/cms/store/user/paus"
-
 export CINDERELLA_DIR="/usr/local/DynamicData/Cinderella"
 export CINDERELLA_LOGDIR="/var/log/DynamicData/Cinderella"
